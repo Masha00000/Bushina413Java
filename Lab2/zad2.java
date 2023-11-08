b9d1a100506bf2c1bea50b04d66dbb998f645475
Masha00000 <143980697+Masha00000@users.noreply.github.com>
2023-11-08 16:45:32 +0700

    Update zad2.java

--- Lab2/zad2.java
+++ Lab2/zad2.java
@@ -1,15 +1,19 @@
 package Lab2;
-import java.math.BigInteger;
-import java.util.Scanner;
-public class zad2 {
-    public static void main(String[] args) {
-        java.util.Scanner scanner = new Scanner(System.in);
-        System.out.print("N?");
 
+import java.util.Scanner;
+// Составить программу расчёта факториала для произвольного числа n<10
+public class zad2 {
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число от 1 до 10");
         int f = scanner.nextInt();
-        int rez = 1 ;
-        for (int i=1; i<=f; i++)
-        {rez*=i;
-            System.out.println(rez);}
+        int rez = 1;
+
+        for (int i = 1; i <= f; i++) {
+            rez *= i;
+            System.out.println("Факториал = " + rez);
+        }
     }
 }
